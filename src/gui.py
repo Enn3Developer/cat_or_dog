381b785678ce42c75ede8c5db97b4a0cac30cb8d
Enn3Developer <arturpeshko39@gmail.com>
2021-11-15 19:16:14 +0100

    Comments Translated comments to let Italian middle schoolers what's happening in the code

--- src/gui.py
+++ src/gui.py
@@ -24,12 +24,12 @@ root.iconphoto(False, tk.PhotoImage(file="logo.ico"))
 root.columnconfigure(0, weight=1)
 
 
-# GUI Title
+# Titolo della GUI
 titleTxt = ttk.Label(root, text=title, font=("Futura", 24, "bold"))
 titleTxt.grid(pady=(10, 15))
 
 
-# Display img on the GUI
+# Mostra immagini sulla GUI
 def addImg(source):
     img = Image.open(source)
     img = img.resize((300, 300), Image.ANTIALIAS)
@@ -39,9 +39,9 @@ def addImg(source):
     panel.image = img
 
 
-addImg("empty.png")  # Init the img frame
+addImg("empty.png")  # Inizializza il frame dell'immagine
 
-# Selct the img from computer
+# Seleziona un'immagine dal computer
 def selectImg():
     file = fd.askopenfilename()
     addImg(file)
@@ -50,19 +50,16 @@ def selectImg():
     acc = prediction[1].cat if prediction[0] == "Cat" else prediction[1].dog
     ai_result(CATEGORIES[prediction[0]])
     ai_percent(acc)
-    # TODO: get predictions from the model
-    # ai_result("Cane")
-    # ai_percent("47%")
 
 
-# Reset the img output
+# Resetta l'immagine
 def clear():
     addImg("empty.png")
     ai_result("Risultato")
     ai_percent("--%")
 
 
-# Display the results
+# Mostra i risultati
 result_default = tk.StringVar()
 result_default.set("Risultato")
 
@@ -71,7 +68,7 @@ def ai_result(result):
     result_default.set(result)
 
 
-# Display the percentage
+# Mostra la percentuale
 perc_default = tk.StringVar()
 perc_default.set("--%")
 
@@ -80,28 +77,28 @@ def ai_percent(perc):
     perc_default.set(f"{perc*100: .2f}%")
 
 
-# Select Image button
+# Bottone Select Image
 select_button = ttk.Button(root, text="Select Image", width=25, command=selectImg).grid(
     pady=(25, 3)
 )
 
-# Reset Image button
+# Bottone Reset Image
 clear_button = ttk.Button(root, text="Clear", width=25, command=clear).grid()
 
 
-# Label for results
+# Targhetta per i risultati
 aiLabel = ttk.Label(
     root, textvariable=result_default, font=("Futura", 18), width=12
 ).grid(pady=(20, 0))
 
 
-# Label for percentage
+# Targhetta per le percentuali
 aiPercent = ttk.Label(
     root, textvariable=perc_default, font=("Futura", 18), width=12
 ).grid(pady=(0, 10))
 
 
-# Credits label
+# Crediti
 creditsTxt = ttk.Label(
     root,
     text=title + " v" + app_ver + " - " + authors,
